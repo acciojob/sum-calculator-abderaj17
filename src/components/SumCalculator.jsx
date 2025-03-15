1dc3c6c0db436b2d535c03d171ce79b938fc219e
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-03-15 13:45:39 +0530

    com

---
+++ src/components/SumCalculator.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SumCalculator = () => {
+  return (
+    <div>
+        <h1>Sum Calculator</h1>
+        <br/>
+        <input type='number' id='num' />
+        <br/>
+
+        <div className='result'>Sum:</div>
+    </div>
+  )
+}
+
+export default SumCalculator
